4cb097724e47e7f6f18ca9fb3256b31eb3cc7603
Juan Pablo Prada <jpradachavaro@gmail.com>
2026-02-10 17:45:17 -0500

    Implementacion clase socio como usuario

---
+++ src/main/java/org/yourcompany/yourproject/Socio.java
@@ -0,0 +1,22 @@
+package org.yourcompany.yourproject;
+
+public class Socio extends usuario {
+    private int limiteEjemplares;
+    private int ejemplaresEnPrestamo;
+    private double multasPendientes;
+    public Socio(String id, String nombre, String email, int limiteEjemplares) {
+        super(id, nombre, email);
+        this.limiteEjemplares = limiteEjemplares;
+        ejemplaresEnPrestamo = 0;
+        multasPendientes = 0;
+    }
+    public void registrarPrestamo(int cantidad){
+        if(cantidad > (limiteEjemplares - ejemplaresEnPrestamo)){
+            throw new IllegalArgumentException("El socio supera el limite");
+        }
+        if(multasPendientes > 0){
+            throw new IllegalArgumentException("El socio tiene multas pendientes, no puede pedir prestamos");
+        }
+        System.out.println("Se prestaron" + cantidad + " Libros");
+    }
+}
