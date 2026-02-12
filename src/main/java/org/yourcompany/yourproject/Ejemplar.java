d47edffe3e2c7bab6fe06a2d4eb42ce9e4a76f60
danielramirezzz <daniel.ramirez2@estudiantesunibague.edu.co>
2026-02-12 17:35:35 -0500

    Se creó la clase ejemplar

---
+++ src/main/java/org/yourcompany/yourproject/Ejemplar.java
@@ -0,0 +1,16 @@
+package org.yourcompany.yourproject;
+
+public class Ejemplar {
+
+    private String codigo;
+    //Libro libro = new Libro();
+
+
+    public String darCodigo() {
+        return codigo;
+    }
+
+    public Libro darLibro() {
+        return libro;
+    }
+}
