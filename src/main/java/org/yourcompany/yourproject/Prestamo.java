0de674cb46ad23827dccd3d53508d63e1a9c8bf5
Cristopher Alejandro Santana Lugo <casantanal@centromotosdelhuila.com>
2026-02-12 17:38:26 -0500

    Préstamo

---
+++ src/main/java/org/yourcompany/yourproject/Prestamo.java
@@ -0,0 +1,15 @@
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Prestamo {
+    private Socio socio;
+    private ArrayList<Ejemplar> = new ArrayList<>();
+
+    public void pedirPrestamo(Socio socio, Ejemplar ArrayList<Ejemplar>){
+
+    }
+    public void notificarCreacion(Notificador notificador){
+
+    }
+}
