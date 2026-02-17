58af651d0022cae868a3fcd052b87611ffb7c3d2
danielramirezzz <daniel.ramirez2@estudiantesunibague.edu.co>
2026-02-17 16:34:50 -0500

    Prueba Daniel

--- src/main/java/org/yourcompany/yourproject/Ejemplar.java
+++ src/main/java/org/yourcompany/yourproject/Ejemplar.java
@@ -5,6 +5,10 @@ public class Ejemplar {
     private String codigo;
     private Libro libro;
 
+    public Ejemplar(String codigo, Libro libro) {
+        this.codigo = codigo;
+        this.libro = libro;
+    }
 
     public String darCodigo() {
 
